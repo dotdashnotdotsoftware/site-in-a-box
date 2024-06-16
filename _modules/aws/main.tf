cb216a30f23997d0805648f697df9dfc9418251a
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2024-06-16 16:16:43 +0200

    fix | Fix badly named resource

--- _modules/aws/main.tf
+++ _modules/aws/main.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
 }
 
 data "aws_caller_identity" "current" {}
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   bucket = aws_s3_bucket.main_bucket.id
   policy = jsonencode({
     "Version": "2012-10-17",
